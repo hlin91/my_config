8d3f6446c4c127198e1ddc4db495b09c342bef49
hlin91 <hlin91@outlook.com>
2021-06-26 15:41:23 -0700

    Added alias for downloads folder

--- profile.sh
+++ profile.sh
@@ -1,7 +1,8 @@
 PATH=$PATH:~/bin:~/go/bin:/usr/local/lib/ruby/gems/2.7.0/bin
-projects=/Volumes/HP/school/coding
+projects=/Volumes/HP/coding
 voice=/Volumes/HP/music/ボイス
 school=/Volumes/HP/school
 calpoly=$school/calpoly
 jav=/Volumes/JAV
+downloads=/Volumes/HP/@downloads
 EDITOR=emacs
