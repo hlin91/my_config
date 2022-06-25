5a70841bd6079d6f9835daea52183fe997b92411
hlin91 <hlin91@outlook.com>
2022-06-24 17:53:56 -0700

    Removing unused stuff

--- install.sh
+++ install.sh
@@ -3,5 +3,3 @@
 ln -sf $(pwd)/emacs_config.el ~/.emacs
 ln -sf $(pwd)/profile.sh ~/.profile
 ln -sf $(pwd)/zsh_config.sh ~/.zshrc
-
-cp -rf ./emacs-color-theme-solarized ~/.emacs.d/emacs-color-theme-solarized
