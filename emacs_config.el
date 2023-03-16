e91837e9cb0baa32270d14e4e79d214fda795cce
hlin91 <hlin91@outlook.com>
2023-03-16 14:44:46 -0700

    Add function to adjust window size on start

--- emacs_config.el
+++ emacs_config.el
@@ -106,11 +106,30 @@ There are two things you can do about this warning:
     ))
 (add-hook 'window-setup-hook 'on-after-init)
 
+(defun set-frame-size-according-to-resolution ()
+  (interactive)
+  (if (display-graphic-p (selected-frame))
+  (progn
+    ;; use 120 char wide window for largeish displays
+    ;; and smaller 80 column windows for smaller displays
+    ;; pick whatever numbers make sense for you
+    (if (> (x-display-pixel-width) 1280)
+           (add-to-list 'default-frame-alist (cons 'width 240))
+           (add-to-list 'default-frame-alist (cons 'width 100)))
+    ;; for the height, subtract a couple hundred pixels
+    ;; from the screen height (for panels, menubars and
+    ;; whatnot), then divide by the height of a char to
+    ;; get the height we want
+    (add-to-list 'default-frame-alist 
+         (cons 'height (/ (- (x-display-pixel-height) 200)
+                             (frame-char-height)))))))
+
 ;; Configurations specific to windowed mode
 (if (display-graphic-p (selected-frame))
     (progn
       (setq x-select-enable-clipboard 1)
-      (server-start)))
+      (server-start)
+      (set-frame-size-according-to-resolution)))
 
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
