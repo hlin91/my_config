68414cab90688c28d49e7d77d2a61dbdf0596b25
hlin91 <hlin91@outlook.com>
2023-05-26 23:57:08 -0700

    Updating emacs config

--- emacs_config.el
+++ emacs_config.el
@@ -29,6 +29,7 @@ There are two things you can do about this warning:
  '(catppuccin-flavor 'frappe)
  '(custom-safe-themes
    '("8d69d4efbc29aa7ad70fc162447576c0327262c3e6baa8d7a41c90b981cc93f9" "276c08753eae8e13d7c4f851432b627af58597f2d57b09f790cb782f6f043966" "bb6d3df2670c74f4ad513512feea89cdbe3040c1d0ba00cf641d63679ee3d0fc" "5963c22b5f105090a406f6a9eff7e61aa4c64b564cabf195f3021e4752b35132" "fbd91b2e6dc5c7912e86406226638adb014612386516e078a0447195591447d3" "7ea883b13485f175d3075c72fceab701b5bf76b2076f024da50dff4107d0db25" "ba72dfc6bb260a9d8609136b9166e04ad0292b9760a3e2431cf0cd0679f83c3a" "41098e2f8fa67dc51bbe89cce4fb7109f53a164e3a92356964c72f76d068587e" "467dc6fdebcf92f4d3e2a2016145ba15841987c71fbe675dcfe34ac47ffb9195" "a138ec18a6b926ea9d66e61aac28f5ce99739cf38566876dc31e29ec8757f6e2" "51c71bb27bdab69b505d9bf71c99864051b37ac3de531d91fdad1598ad247138" "8a379e7ac3a57e64de672dd744d4730b3bdb88ae328e8106f95cd81cbd44e0b6" "2035a16494e06636134de6d572ec47c30e26c3447eafeb6d3a9e8aee73732396" "ae426fc51c58ade49774264c17e666ea7f681d8cae62570630539be3d06fd964" "89d9dc6f4e9a024737fb8840259c5dd0a140fd440f5ed17b596be43a05d62e67" "00cec71d41047ebabeb310a325c365d5bc4b7fab0a681a2a108d32fb161b4006" "be84a2e5c70f991051d4aaf0f049fa11c172e5d784727e0b525565bb1533ec78" "7a424478cb77a96af2c0f50cfb4e2a88647b3ccca225f8c650ed45b7f50d9525" "5b9a45080feaedc7820894ebbfe4f8251e13b66654ac4394cb416fef9fdca789" "2721b06afaf1769ef63f942bf3e977f208f517b187f2526f0e57c1bd4a000350" "02f57ef0a20b7f61adce51445b68b2a7e832648ce2e7efb19d217b6454c1b644" "631c52620e2953e744f2b56d102eae503017047fb43d65ce028e88ef5846ea3b" "680f62b751481cc5b5b44aeab824e5683cf13792c006aeba1c25ce2d89826426" "4ff1c4d05adad3de88da16bd2e857f8374f26f9063b2d77d38d14686e3868d8d" "7e377879cbd60c66b88e51fad480b3ab18d60847f31c435f15f5df18bdb18184" "991ca4dbb23cab4f45c1463c187ac80de9e6a718edc8640003892a2523cb6259" "adaf421037f4ae6725aa9f5654a2ed49e2cd2765f71e19a7d26a454491b486eb" "8146edab0de2007a99a2361041015331af706e7907de9d6a330a3493a541e5a6" "e8df30cd7fb42e56a4efc585540a2e63b0c6eeb9f4dc053373e05d774332fc13" "333958c446e920f5c350c4b4016908c130c3b46d590af91e1e7e2a0611f1e8c5" "cf922a7a5c514fad79c483048257c5d8f242b21987af0db813d3f0b138dfaf53" "3d54650e34fa27561eb81fc3ceed504970cc553cfd37f46e8a80ec32254a3ec3" "246a9596178bb806c5f41e5b571546bb6e0f4bd41a9da0df5dfbca7ec6e2250c" "1704976a1797342a1b4ea7a75bdbb3be1569f4619134341bd5a4c1cfb16abad4" "b186688fbec5e00ee8683b9f2588523abdf2db40562839b2c5458fcfb322c8a4" "aba75724c5d4d0ec0de949694bce5ce6416c132bb031d4e7ac1c4f2dbdd3d580" "8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "3e200d49451ec4b8baa068c989e7fba2a97646091fd555eca0ee5a1386d56077" "833ddce3314a4e28411edf3c6efde468f6f2616fc31e17a62587d6a9255f4633" "d89e15a34261019eec9072575d8a924185c27d3da64899905f8548cbd9491a36" "fee7287586b17efbfda432f05539b58e86e059e78006ce9237b8732fde991b4c" "171d1ae90e46978eb9c342be6658d937a83aaa45997b1d7af7657546cae5985b" "7922b14d8971cce37ddb5e487dbc18da5444c47f766178e5a4e72f90437c0711" "0598c6a29e13e7112cfbc2f523e31927ab7dce56ebb2016b567e1eff6dc1fd4f" "7f1d414afda803f3244c6fb4c2c64bea44dac040ed3731ec9d75275b9e831fe5" "13a8eaddb003fd0d561096e11e1a91b029d3c9d64554f8e897b2513dbf14b277" "830877f4aab227556548dc0a28bf395d0abe0e3a0ab95455731c9ea5ab5fe4e1" "51ec7bfa54adf5fff5d466248ea6431097f5a18224788d0bd7eb1257a4f7b773" "c433c87bd4b64b8ba9890e8ed64597ea0f8eb0396f4c9a9e01bd20a04d15d358" "2809bcb77ad21312897b541134981282dc455ccd7c14d74cc333b6e549b824f3" "669e02142a56f63861288cc585bee81643ded48a19e36bfdf02b66d745bcc626" "efeac8a7737b192859b0683bb97a5c2e4c600101dccda67b78a424fc9a738b75" "4d553fbd7fa02bedfb17e8107680f56e1aa952c073f389c780a5aeaa4896867a" default))
+ '(eglot-confirm-server-initiated-edits nil)
  '(global-display-line-numbers-mode t)
  '(inhibit-startup-screen t)
  '(line-number-mode nil)
@@ -100,6 +101,7 @@ There are two things you can do about this warning:
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
+(setq byte-compile-warnings '(cl-functions))
 
 (defun delete-inside-char (char)
   (interactive (list (read-char-from-minibuffer "Delete inside char: "
@@ -116,8 +118,6 @@ There are two things you can do about this warning:
 (global-set-key (kbd "M-p") #'backward-paragraph)
 (global-set-key (kbd "M-n") #'forward-paragraph)
 (global-set-key (kbd "C-c d") #'delete-inside-char)
-
-(setq byte-compile-warnings '(cl-functions))
 
 ;; Set up use-package for auto-installing packages
 (unless (package-installed-p 'use-package)
@@ -186,7 +186,7 @@ There are two things you can do about this warning:
 (use-package minimap
   :if (display-graphic-p)
   :autoload minimap-mode
-  :config
+  :init
   (setq minimap-width-fraction 0.01)
   (setq minimap-window-location 'right)
   (setq minimap-minimum-width 10))
@@ -232,14 +232,10 @@ There are two things you can do about this warning:
 
 (global-set-key (kbd "C-c C-k") #'comment-or-uncomment-region)
 
-(use-package ace-jump-mode
-  :bind
-  (("C-c C-SPC" . ace-jump-mode)))
-
 (use-package avy
   :bind
-  (("C-:" . avy-goto-char)
-   ("C-'" . avy-goto-char-2)
+  (("C-c C-SPC" . avy-goto-char)
+   ("C-c M-SPC" . avy-goto-char-2)
    ("M-g M-f" . avy-goto-line)
    ("M-g M-w" . avy-goto-word-1)
    ("M-g M-e" . avy-goto-word-0)))
@@ -329,6 +325,10 @@ There are two things you can do about this warning:
 (use-package git-gutter
   :config
   (global-git-gutter-mode 1))
+
+(use-package blamer
+  :bind
+  (("C-c b" . blamer-show-commit-info)))
 
 (use-package projectile
   :defer t)
@@ -449,14 +449,6 @@ There are two things you can do about this warning:
 	    (local-set-key (kbd "<f18>") 'artist-select-op-square)   ; f18 = rectangle
 	    (local-set-key (kbd "<f19>") 'artist-select-op-ellipse)  ; f19 = ellipse
 	    (local-set-key (kbd "C-z") 'undo)))
-
-;; org-mode keybindings
-(add-hook 'org-mode-hook
-          (lambda ()
-            (local-set-key (kbd "C-l") 'org-store-link)
-            (local-set-key (kbd "C-a") 'org-agenda)
-            (local-set-key (kbd "C-c") 'org-capture)
-            (local-set-key (kbd "C-b") 'org-switchb)))
 
 (provide '.emacs)
 ;;; .emacs ends here
