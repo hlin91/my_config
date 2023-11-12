6254f8cee2cf891501505461b1a53e7817585d06
hlin91 <hlin91@outlook.com>
2023-11-12 12:24:35 -0800

    Deleting old config files

--- install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ln -sf $(pwd)/emacs_config.el ~/.emacs
-ln -sf $(pwd)/profile.sh ~/.profile
-ln -sf $(pwd)/zsh_config.sh ~/.zshrc
--- profile.sh
+++
@@ -1,9 +0,0 @@
-PATH=$PATH:~/bin:~/go/bin:/usr/local/lib/ruby/gems/2.7.0/bin
-projects=/Volumes/HP/coding
-voice=/Volumes/HP/music/ボイス
-school=/Volumes/HP/school
-calpoly=$school/calpoly
-jav=/Volumes/JAV
-downloads=/Volumes/HP/@downloads
-EDITOR=emacs
-. "$HOME/.cargo/env"
